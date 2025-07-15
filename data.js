0e5c56b94c2012741dc9dd0f4f0f22f0da1933a8
Stefan <stefan.maxwell@gmail.com>
2025-07-15 21:51:01 +0100

    Add additional sequences

--- data.js
+++ data.js
@@ -68,4 +68,165 @@ const sequences = [
     ],
     correct: "Flower",
   },
+  {
+    steps: [
+      {
+        text: "Paint",
+        image: "./assets/images/paint.png",
+      },
+      {
+        text: "Paintbrush",
+        image: "./assets/images/paintbrush.png",
+      },
+    ],
+    options: [
+      {
+        text: "Painting",
+        image: "./assets/images/painting.png",
+      },
+      {
+        text: "Shoes",
+        image: "./assets/images/shoes.png",
+      },
+    ],
+    correct: "Painting",
+  },
+  {
+    steps: [
+      {
+        text: "Tadpole",
+        image: "./assets/images/tadpole.png",
+      },
+      {
+        text: "Lily Pad",
+        image: "./assets/images/lily-pad.png",
+      },
+    ],
+    options: [
+      {
+        text: "Frog",
+        image: "./assets/images/frog.png",
+      },
+      {
+        text: "Cloud",
+        image: "./assets/images/cloud.png",
+      },
+    ],
+    correct: "Frog",
+  },
+  {
+    steps: [
+      {
+        text: "Dirty Plate",
+        image: "./assets/images/dirty-plate.png",
+      },
+      {
+        text: "Sponge",
+        image: "./assets/images/sponge.png",
+      },
+    ],
+    options: [
+      {
+        text: "Clean Plate",
+        image: "./assets/images/clean-plate.png",
+      },
+      {
+        text: "Lamp",
+        image: "./assets/images/lamp.png",
+      },
+    ],
+    correct: "Clean Plate",
+  },
+  {
+    steps: [
+      {
+        text: "Flour",
+        image: "./assets/images/flour.png",
+      },
+      {
+        text: "Milk",
+        image: "./assets/images/milk.png",
+      },
+    ],
+    options: [
+      {
+        text: "Cake",
+        image: "./assets/images/cake.png",
+      },
+      {
+        text: "Rock",
+        image: "./assets/images/rock.png",
+      },
+    ],
+    correct: "Cake",
+  },
+  {
+    steps: [
+      {
+        text: "Wood",
+        image: "./assets/images/wood.png",
+      },
+      {
+        text: "Hammer",
+        image: "./assets/images/hammer.png",
+      },
+    ],
+    options: [
+      {
+        text: "Bookshelves",
+        image: "./assets/images/bookshelves.png",
+      },
+      {
+        text: "Fish",
+        image: "./assets/images/fish.png",
+      },
+    ],
+    correct: "Bookshelves",
+  },
+  {
+    steps: [
+      {
+        text: "Ice Cube",
+        image: "./assets/images/ice-cube.png",
+      },
+      {
+        text: "Sun",
+        image: "./assets/images/sun.png",
+      },
+    ],
+    options: [
+      {
+        text: "Puddle",
+        image: "./assets/images/puddle.png",
+      },
+      {
+        text: "Tree",
+        image: "./assets/images/tree.png",
+      },
+    ],
+    correct: "Puddle",
+  },
+  {
+    steps: [
+      {
+        text: "Key",
+        image: "./assets/images/key.png",
+      },
+      {
+        text: "Lock",
+        image: "./assets/images/lock.png",
+      },
+    ],
+    options: [
+      {
+        text: "Open Door",
+        image: "./assets/images/open-door.png",
+      },
+      {
+        text: "Hat",
+        image: "./assets/images/hat.png",
+      },
+    ],
+    correct: "Open Door",
+  },
 ];
