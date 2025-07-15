57c6e53e09358d7bf22d245351b72d7764163445
Stefan <stefan.maxwell@gmail.com>
2025-07-15 19:06:34 +0100

    Add seeds image; Reduce data set to those with images

--- data.js
+++ data.js
@@ -49,8 +49,7 @@ const sequences = [
     steps: [
       {
         text: "Seeds",
-        image:
-          "https://images.pexels.com/photos/300859/pexels-photo-300859.jpeg?auto=compress&cs=tinysrgb&w=600",
+        image: "./assets/images/seeds.png",
       },
       {
         text: "Watering Can",
@@ -68,86 +67,5 @@ const sequences = [
       },
     ],
     correct: "Flower",
-  },
-  {
-    steps: [
-      {
-        text: "Empty Cup",
-        image:
-          "https://images.pexels.com/photos/302896/pexels-photo-302896.jpeg?auto=compress&cs=tinysrgb&w=600",
-      },
-      {
-        text: "Kettle",
-        image:
-          "https://images.pexels.com/photos/1459339/pexels-photo-1459339.jpeg?auto=compress&cs=tinysrgb&w=600",
-      },
-    ],
-    options: [
-      {
-        text: "Cup of Tea",
-        image:
-          "https://images.pexels.com/photos/2304895/pexels-photo-2304895.jpeg?auto=compress&cs=tinysrgb&w=600",
-      },
-      {
-        text: "Banana",
-        image:
-          "https://images.pexels.com/photos/5966630/pexels-photo-5966630.jpeg?auto=compress&cs=tinysrgb&w=600",
-      },
-    ],
-    correct: "Cup of Tea",
-  },
-  {
-    steps: [
-      {
-        text: "Toothbrush",
-        image:
-          "https://images.pexels.com/photos/4223693/pexels-photo-4223693.jpeg?auto=compress&cs=tinysrgb&w=600",
-      },
-      {
-        text: "Toothpaste",
-        image:
-          "https://images.pexels.com/photos/4465124/pexels-photo-4465124.jpeg?auto=compress&cs=tinysrgb&w=600",
-      },
-    ],
-    options: [
-      {
-        text: "Brushing Teeth",
-        image:
-          "https://images.pexels.com/photos/6479573/pexels-photo-6479573.jpeg?auto=compress&cs=tinysrgb&w=600",
-      },
-      {
-        text: "Key",
-        image:
-          "https://images.pexels.com/photos/139312/pexels-photo-139312.jpeg?auto=compress&cs=tinysrgb&w=600",
-      },
-    ],
-    correct: "Brushing Teeth",
-  },
-  {
-    steps: [
-      {
-        text: "Letter",
-        image:
-          "https://images.pexels.com/photos/257853/pexels-photo-257853.jpeg?auto=compress&cs=tinysrgb&w=600",
-      },
-      {
-        text: "Envelope",
-        image:
-          "https://images.pexels.com/photos/221047/pexels-photo-221047.jpeg?auto=compress&cs=tinysrgb&w=600",
-      },
-    ],
-    options: [
-      {
-        text: "Mailbox",
-        image:
-          "https://images.pexels.com/photos/288949/pexels-photo-288949.jpeg?auto=compress&cs=tinysrgb&w=600",
-      },
-      {
-        text: "Chair",
-        image:
-          "https://images.pexels.com/photos/2762247/pexels-photo-2762247.jpeg?auto=compress&cs=tinysrgb&w=600",
-      },
-    ],
-    correct: "Mailbox",
   },
 ];
